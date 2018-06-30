e95a65081251fddfecf2a066e287b8e922b5484f
YongSung Lee <yslee@humminglab.io>
2018-06-30 12:28:04 +0900

    define message body to implement more

--- skdemo/gmmp.h
+++ skdemo/gmmp.h
@@ -8,13 +8,23 @@
 
 #include "smarthome_conf.h"
 
-#define HEARTBEAT_INTERVAL	60
+#define HEARTBEAT_INTERVAL	600
+
+#define MEDIA_TYPE_APPLICATION_JSON	0x65
 
 typedef enum {
     GMMP_GW_REG_REQ		= 0x01,
     GMMP_GW_REG_RESP		= 0x02,
     GMMP_GW_DEREG_REQ		= 0x03,
     GMMP_GW_DEREG_RESP		= 0x04,
+    GMMP_PROFILE_REQ		= 0x05,
+    GMMP_PROFILE_RESP		= 0x06,
+    GMMP_DEV_REG_REQ		= 0x07,
+    GMMP_DEV_REG_RESP		= 0x08,
+    GMMP_DEV_DEREG_REQ		= 0x09,
+    GMMP_DEV_DEREG_RESP		= 0x0a,
+    GMMP_DELIVERY_REQ		= 0x0b,
+    GMMP_DELIVERY_RESP		= 0x0c,
     GMMP_CTRL_REQ		= 0x0d,
     GMMP_CTRL_RESP		= 0x0e,
     GMMP_HEARTBEAT_REQ		= 0x0f,
@@ -43,12 +53,12 @@ typedef struct {
     uint8_t reserved;		/* Reserved */
 } __attribute__((packed)) gmmp_header_t;
 
-typedef struct _gw_reg_req_t {
+typedef struct {
     char domain_code[LEN_DOMAIN_CODE];
     char manufacture_id[LEN_MANUFACTURE_ID];
 } __attribute__((packed)) gw_reg_req_t;
 
-typedef struct _gw_reg_resp_t {
+typedef struct {
     char domain_code[LEN_DOMAIN_CODE];
     char gw_id[LEN_GW_ID];
     uint8_t result_code;
@@ -57,11 +67,61 @@ typedef struct _gw_reg_resp_t {
 typedef struct {
     char domain_code[LEN_DOMAIN_CODE];
     char gw_id[LEN_GW_ID];
+    char manufacture_id[LEN_MANUFACTURE_ID];
+} __attribute__((packed)) dev_reg_req_t;
+
+typedef struct {
+    char domain_code[LEN_DOMAIN_CODE];
+    char gw_id[LEN_GW_ID];
+    char device_id[16];
+    uint8_t result_code;
+} __attribute__((packed)) dev_reg_resp_t;
+
+typedef struct {
+    char domain_code[LEN_DOMAIN_CODE];
+    char gw_id[LEN_GW_ID];
+    char device_id[16];
+} __attribute__((packed)) profile_req_t;
+
+typedef struct {
+    char domain_code[LEN_DOMAIN_CODE];
+    char gw_id[LEN_GW_ID];
+    char device_id[16];
+    uint8_t result_code;
+    uint32_t heartbeat_period;
+    uint32_t report_period;
+    uint32_t report_offset;
+    uint32_t response_timeout;
+    char model[32];
+    uint16_t firmware_version;
+    uint16_t software_version;
+    uint16_t hardware_version;
+} __attribute__((packed)) profile_resp_t;
+
+typedef struct {
+    char domain_code[LEN_DOMAIN_CODE];
+    char gw_id[LEN_GW_ID];
+    char device_id[16];
+    uint8_t report_time;
+    uint8_t media_type;
+} __attribute__((packed)) delivery_req_t;
+
+typedef struct {
+    char domain_code[LEN_DOMAIN_CODE];
+    char gw_id[LEN_GW_ID];
+    char device_id[16];
+    uint8_t result_code;
+    uint32_t backoof_time;
+} __attribute__((packed)) delivery_resp_t;
+
+typedef struct {
+    char domain_code[LEN_DOMAIN_CODE];
+    char gw_id[LEN_GW_ID];
     char device_id[16];
     uint8_t control_type;
 } __attribute__((packed)) ctrl_req_t;
 
-typedef struct _ctrl_resp_t {
+typedef struct {
     char domain_code[LEN_DOMAIN_CODE];
     char gw_id[LEN_GW_ID];
     char device_id[16];
@@ -69,7 +129,7 @@ typedef struct _ctrl_resp_t {
     uint8_t result_code;
 } __attribute__((packed)) ctrl_resp_t;
 
-typedef struct _heartbeat_req_t {
+typedef struct {
     char domain_code[LEN_DOMAIN_CODE];
     char gw_id[LEN_GW_ID];
 } __attribute__((packed)) heartbeat_req_t;
@@ -103,12 +163,13 @@ typedef struct {
     uint8_t result_code;
 } __attribute__((packed)) set_enc_key_resp_t;
 
-typedef struct _gw_reg_req_t gw_dereg_req_t;
-typedef struct _gw_reg_resp_t gw_dereg_resp_t;
-typedef struct _ctrl_resp_t ctrl_noti_t;
-typedef struct _ctrl_resp_t  ctrl_noti_resp_t;
-typedef struct _heartbeat_req_t heartbeat_resp_t;
-
+typedef gw_reg_req_t gw_dereg_req_t;
+typedef gw_reg_resp_t gw_dereg_resp_t;
+typedef ctrl_resp_t ctrl_noti_t;
+typedef ctrl_resp_t  ctrl_noti_resp_t;
+typedef heartbeat_req_t heartbeat_resp_t;
+typedef profile_req_t dev_dereg_req_t;
+typedef dev_reg_resp_t dev_dereg_resp_t;
 
 OSStatus read_gmmp_frame( int fd, void *buf, size_t *size );
 size_t fill_reg_req( void* buf );
